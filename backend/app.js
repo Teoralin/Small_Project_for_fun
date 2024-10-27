1c7d37208ddfdf7a334b5dcfe776b0efd86e5378
Miffiny <nesbaraniyk@gmail.com>
2024-10-27 13:24:54 +0100

    test add

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const dbUtils = require('./mysql_header');
 
 async function main() {
-    //const user = await dbUtils.insertUser("AliceInWonderland", "aliceinwonderlend@example.com", "superpassword", "Alice", "rabbit hole", "Registered user");
-    const user = await dbUtils.getItemByColumn("user", "name", "AliceInWonderland");
-    const wasRegistered = await dbUtils.registerUser(user.user_id, 0);
-    console.log(wasRegistered);
-    
+ //   const user = await dbUtils.insertUser("AliceInWonderland", "aliceinwonderlend@example.com", "superpassword", "Alice", "rabbit hole", "Registered user");
+    const [user] = await dbUtils.getItemByColumn("user", "name", "Alice");
+    console.log(user);
+    const wasregistered = await dbUtils.registerUser(user.user_id, 0);
+    console.log(wasregistered);
 }
 
 // Use an IIFE (Immediately Invoked Function Expression) to await main and closeConnection sequentially
