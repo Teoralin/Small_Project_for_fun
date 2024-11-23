2c5d6dd0d3e72ebbd5a54de9ded9ab682e4a766c
Nadzeya <xantsi00@vutbr.cz>
2024-11-23 22:46:50 +0100

    dt

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ async function initializeApp() {
         console.log('Database connection has been established successfully.');
 
         // Synchronize models with the database
-        await sequelize.sync({force: true});
+        await sequelize.sync({alter: true});
         console.log('Models synchronized with the database.');
 
         // Start the server
