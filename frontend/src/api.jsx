2483a2a12f2a9ab946c8f991f2f58314d1536e27
Nadzeya <xantsi00@vutbr.cz>
2024-11-24 00:00:46 +0100

    port

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // Replace with your API base URL
+    baseURL: 'https://backend-4g4p.onrender.com:3000', // Replace with your API base URL
     timeout: 10000,                  // Set a timeout for requests (optional)
 });
 
