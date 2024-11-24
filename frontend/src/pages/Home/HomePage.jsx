c12bcc7827caac88de8027a21614813ea22167e7
Anastasiia Chumachenko <xchuma00@vutbr.cz>
2024-11-24 16:08:15 +0100

    HomePage small fix

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import classes from "./HomePage.module.css";
-//import userIcon from "../../assets/User.png";
 import time from "../../assets/time-eat.png";
 import delivery from "../../assets/delivery.png";
 import paris from "../../assets/paris.png";
@@ -11,7 +10,6 @@ import insta_3 from "../../assets/Insta_3.png";
 import insta_4 from "../../assets/Insta_4.png";
 import insta_5 from "../../assets/Insta_5.png";
 import {Link, useNavigate} from "react-router-dom";
-import hand from "../../assets/Harvest_icon.png";
 import {jwtDecode} from 'jwt-decode';
 import api from "../../api";
 
@@ -31,7 +29,6 @@ export default function HomePage() {
         navigate(path);
     };
 
-    // Decode token and check the user's role
     useEffect(() => {
         const token = localStorage.getItem("token");
         console.log(token);
@@ -41,7 +38,7 @@ export default function HomePage() {
                 setUserRole(decodedToken.role); 
             } catch (error) {
                 console.error("Invalid token:", error);
-                localStorage.removeItem("token"); // Remove invalid token
+                localStorage.removeItem("token");
             }
         }
     }, []);
@@ -58,207 +55,169 @@ export default function HomePage() {
     };
 
     useEffect(() => {
-        // Fetch users from the API using axios
         async function fetchUsers() {
             try {
                 const response = await api.get('/users');  
 
-                // Filter users who have 'is_farmer' set to true
                 const farmerUsers = response.data.filter(user => user.is_farmer === true);
 
-                setUsers(farmerUsers); // Set filtered users
-                setFilteredUsers(farmerUsers); // Initialize filteredUsers
+                setUsers(farmerUsers);
+                setFilteredUsers(farmerUsers);
             } catch (error) {
                 console.error('Error fetching users:', error);
-                setError('An error occurred while fetching users'); // Set error state
+                setError('An error occurred while fetching users');
             } finally {
-                setLoading(false); // Set loading to false once the request is complete
+                setLoading(false);
             }
         }
 
         fetchUsers();
-    }, []); // Empty dependency array to run the effect once on mount
+    }, []);
 
     const handleScrollLeft = () => {
-        setScrollPosition(scrollPosition - 300); // Прокручиваем влево
+        setScrollPosition(scrollPosition - 300);
     };
 
     const handleScrollRight = () => {
-        setScrollPosition(scrollPosition + 300); // Прокручиваем вправо
+        setScrollPosition(scrollPosition + 300);
     };
 
     return (
         <div className={classes.HomePage}>
-            <div className={classes.welcomescreen}>
-                <div className={classes.heading}>Your Food court at home</div>
-                <div className={classes.information}>
-                    <div className={classes.info}>
-                        <h2>Delivery</h2>
-                        <p>Order in</p>
+                <div className={classes.welcomescreen}>
+                    <div className={classes.heading}>Your Food court at home</div>
+                    <div className={classes.information}>
+                        <div className={classes.info} onClick={() => handleNavigate('/categories')}>
+                            <h2>Delivery</h2>
+                            <p>Order in</p>
+                        </div>
+                        <div className={classes.info} onClick={() => handleNavigate('/harvestList')}>
+                            <h2>Self harvest</h2>
+                            <p>Grab and go</p>
+                        </div>
                     </div>
-                    <div className={classes.info}>
-                        <h2>Self harvest</h2>
-                        <p>Grab and go</p>
+
+                    <div className={classes.benefits}>
+                        <div className={classes.benefitItem}>
+                            <img
+                                src={time}
+                                alt="Time Icon"
+                                className={classes.icon}
+                            />
+                            <p>Fast delivery in less than 42 hours</p>
+                        </div>
+                        <div className={classes.benefitItem}>
+                            <img
+                                src={delivery}
+                                alt="Delivery Icon"
+                                className={classes.icon}
+                            />
+                            <p>Free delivery from 999 CZK</p>
+                        </div>
+                        <div className={classes.benefitItem}>
+                            <img
+                                src={paris}
+                                alt="Paris Icon"
+                                className={classes.icon}
+                            />
+                            <p>Only fresh and Czech products</p>
+                        </div>
                     </div>
+
+                    <img
+                        src={food}
+                        alt="Food Photo"
+                        className={`${classes.icon} ${classes.foodIcon}`}
+                    />
                 </div>
 
-                <div className={classes.benefits}>
-                    <div className={classes.benefitItem}>
-                        <img
-                            src={time}
-                            alt="Time Icon"
-                            className={classes.icon}
-                        />
-                        <p>Fast delivery in less than 42 hours</p>
-                    </div>
-                    <div className={classes.benefitItem}>
-                        <img
-                            src={delivery}
-                            alt="Delivery Icon"
-                            className={classes.icon}
-                        />
-                        <p>Free delivery from 999 CZK</p>
-                    </div>
-                    <div className={classes.benefitItem}>
-                        <img
-                            src={paris}
-                            alt="Paris Icon"
-                            className={classes.icon}
-                        />
-                        <p>Only fresh and Czech products</p>
-                    </div>
-                </div>
-
-                <img
-                    src={food}
-                    alt="Food Photo"
-                    className={`${classes.icon} ${classes.foodIcon}`}
-                />
-            </div>
-
-            <div className={classes.farmers}>
-                <div className={classes.compo}>
-                    <p>Farmers</p>
-                    <button
-                        type="button"
-                        className={classes.ShowAllButton}
-                        onClick={() => handleNavigate('/farmers')}
-                    >
-                        Show all
-                    </button>
-                </div>
-
-                {filteredUsers.length === 0 ? (
-                    <p>No users found.</p>
-                ) : (
-                    <div className={classes.scrollContainer}>
+                <div className={classes.farmers}>
+                    <div className={classes.compo}>
+                        <p>Farmers</p>
                         <button
-                            onClick={handleScrollLeft}
-                            className={classes.scrollButton}
-                            disabled={scrollPosition <= 0}
+                            type="button"
+                            className={classes.ShowAllButton}
+                            onClick={() => handleNavigate('/farmers')}
                         >
-                            &#8592; {/* Стрелка влево */}
+                            Show all
                         </button>
-                        <ul
-                            className={classes.UserComponent}
-                            style={{ transform: `translateX(-${scrollPosition}px)` }}
-                        >
-                            {filteredUsers.map((user) => (
-                                <li key={user.user_id} className={classes.userCompo}>
-                                    <div className={classes.UserInfo}>
-                                        {user.name} <br />
-                                        {user.surname}
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                        <button
-                            onClick={handleScrollRight}
-                            className={classes.scrollButton}
-                            disabled={scrollPosition >= (filteredUsers.length - 5) * 300}
-                        >
-                            &#8594; {/* Стрелка вправо */}
-                        </button>
+                    </div>
+
+                    {filteredUsers.length === 0 ? (
+                        <p>No users found.</p>
+                    ) : (
+                        <div className={classes.scrollContainer}>
+                            <button
+                                onClick={handleScrollLeft}
+                                className={classes.scrollButton}
+                                disabled={scrollPosition <= 0}
+                            >
+                                &#8592;
+                            </button>
+                            <ul
+                                className={classes.UserComponent}
+                                style={{transform: `translateX(-${scrollPosition}px)`}}
+                            >
+                                {filteredUsers.map((user) => (
+                                    <li key={user.user_id} className={classes.userCompo}>
+                                        <div className={classes.UserInfo}>
+                                            {user.name} <br/>
+                                            {user.surname}
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                            <button
+                                onClick={handleScrollRight}
+                                className={classes.scrollButton}
+                                disabled={scrollPosition >= (filteredUsers.length - 5) * 300}
+                            >
+                                &#8594;
+                            </button>
+                        </div>
+                    )}
+                </div>
+
+                <div className={classes.instagram}>
+                    <div className={classes.instagram_text}>
+                        <p>#YellowShop </p>
+                        <p> on instagram</p>
+                    </div>
+                    <div className={classes.instagram_img}>
+                        <img
+                            src={insta_1}
+                            alt="insta"
+                            className={classes.icon}
+                        />
+                        <img
+                            src={insta_2}
+                            alt="insta"
+                            className={classes.icon}
+                        />
+                        <img
+                            src={insta_3}
+                            alt="insta"
+                            className={classes.icon}
+                        />
+                        <img
+                            src={insta_4}
+                            alt="insta"
+                            className={classes.icon}
+                        />
+                        <img
+                            src={insta_5}
+                            alt="insta"
+                            className={classes.icon}
+                        />
+                    </div>
+                </div>
+
+                {userRole === "Administrator" && (
+                    <div>
+                        <Link to="/users">Users</Link>
                     </div>
                 )}
             </div>
 
-            <div className={classes.Self_Harvest}>
-                <div className={classes.Harvest_compo}>
-                    <div className={classes.title}>
-                        <p>Self Harvest</p>
-                        <img
-                            src={hand}
-                            alt="hand Icon"
-                            className={classes.icon}
-                        />
-                    </div>
-
-                    <p className={classes.HarvestText}>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor.
-                    </p>
-
-                    <button type="show"
-                            className={classes.ShowAllButton}
-                            onClick={() => handleNavigate('/harvestList')}
-                    >
-                        show all
-                    </button>
-                </div>
-            </div>
-
-            <div className={classes.categories}>
-                <div className={classes.compo}>
-                    <p>Categories</p>
-                    <button type="show"
-                            className={classes.ShowAllButton}
-                            onClick={() => handleNavigate('/categories')}
-                    >
-                        show all
-                    </button>
-                </div>
-            </div>
-
-            <div className={classes.instagram}>
-                <div className={classes.instagram_text}>
-                    <p>#YellowShop </p>
-                    <p> on instagram</p>
-                </div>
-                <div className={classes.instagram_img}>
-                    <img
-                        src={insta_1}
-                        alt="insta"
-                        className={classes.icon}
-                    />
-                    <img
-                        src={insta_2}
-                        alt="insta"
-                        className={classes.icon}
-                    />
-                    <img
-                        src={insta_3}
-                        alt="insta"
-                        className={classes.icon}
-                    />
-                    <img
-                        src={insta_4}
-                        alt="insta"
-                        className={classes.icon}
-                    />
-                    <img
-                        src={insta_5}
-                        alt="insta"
-                        className={classes.icon}
-                    />
-                </div>
-            </div>
-
-            {/* Only render this link if the user is an Administrator */}
-            {userRole === "Administrator" && (
-                <div>
-                <Link to="/users">Users</Link>
-                </div>
-            )}
-        </div>
     );
 }
